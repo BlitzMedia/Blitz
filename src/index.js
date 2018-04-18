fdf6d84a69cdabf3e7d24e78f827b65442ee164c
Sergio <psergiogps@gmail.com>
2018-04-18 13:14:22 +0200

    Adding Rama fonts (gorks) && Web loader

--- src/index.js
+++ src/index.js
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import WebfontLoader from '@dr-kobros/react-webfont-loader';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const fontConfig = {
+  google: {
+    families: ['Dosis:300,400,500'],
+  }
+};
+
+const fontCallback = status => {
+  console.log('Fonts ⇀⎯ ⚡️')
+
+  // I could hook the webfont status to for example Redux here.
+};
+
+
+
+ReactDOM.render(
+  <WebfontLoader config={fontConfig} onStatus={fontCallback}>
+    <App />
+  </WebfontLoader>,
+  document.getElementById('root')
+);
 registerServiceWorker();
