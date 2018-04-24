51f9260d802bc475909b5a05f71fd927b94d944e
gorkolas <gorka.molero@gmail.com>
2018-04-24 13:37:31 +0200

    Correcting line-height and trying out sizes

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import { Heading } from './utils/type'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Heading>Here are Shevy and Emotion</Heading>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="⚡️">
+        <Heading>This is Blitz!</Heading>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id in magnam eum voluptate, placeat, adipisci dolore. Praesentium dicta officia, voluptatum aut quibusdam velit. Molestias, a exercitationem fugiat commodi fuga eos.
         </p>
+        <Heading>This is Blitz! 2</Heading>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut hic, corporis voluptatibus pariatur eum aliquam consequatur molestias omnis, vel quasi voluptatem perferendis iusto expedita. Ab cum quis tempore porro ipsum.</p>
       </div>
     )
   }
--- src/utils/type.js
+++ src/utils/type.js
@@ -2,7 +2,7 @@ import styled, { css } from 'react-emotion'
 import Shevy from 'shevyjs'
 
 const shevyOptions = {
-  baseFontSize: '16px',
+  baseFontSize: '32px',
   baseLineHeight: 1.5,
   baseFontScale: [5.653, 3.998, 2.827, 1.999, 1.414, 1],
   addMarginBottom: true,
@@ -11,9 +11,11 @@ const shevyOptions = {
 }
 
 const shevy = new Shevy(shevyOptions)
+console.log(shevy)
 
 const {
   baseSpacing: bs,
+  lineHeightSpacing: lhs,
   h1: {
     fontSize,
     lineHeight,
@@ -26,8 +28,13 @@ export const Wrap = styled('div')`
   margin-bottom: ${bs(2)};
 `
 
+export const Div = styled('div')`
+  padding: ${bs()};
+`
+
 export const Heading = styled('h1')`
   font-size: ${fontSize};
   line-height: ${lineHeight};
-  margin-bottom: ${marginBottom};
+  margin-bottom: ${lhs(.2)};
+  letter-spacing: .01em;
 `
